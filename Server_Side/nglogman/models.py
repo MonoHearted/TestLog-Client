49d99711d09757ee38603eca8d93a50c68ed712f
ivorchen <ivor.chen@genesys.com>
2019-01-14 17:01:49 -0500

    add data models for nodes and task

--- Server_Side/nglogman/models.py
+++ Server_Side/nglogman/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class LGNode(models.Model):
+    """
+    A data model map a task entity to db
+    """
+    hostname = models.CharField(max_length=200)
+    ip = models.GenericIPAddressField()
+    comments = models.TextField()
+    nodeUUID = models.UUIDField(primary_key=True,null=False,editable=True)
+
+    def __str__(self):
+        return self.hostname
+
+
+class Task(models.Model):
+    taskName = models.CharField(max_length=200,null=False)
+    owner = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    createTime = models.DateTimeField(auto_now=True)
+    startTime = models.DateTimeField(null=False)
+    duration = models.DurationField(null=False)
+    itr = models.PositiveSmallIntegerField(default=4)
+    taskUUID=models.UUIDField(primary_key=True,
+                              default=uuid.uuid4,
+                              editable=False)
+
+    def __str__(self):
+        return self.taskName
